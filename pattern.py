4b1af77876991de11053f2574f9d700b386ef53b
Saurabh-Gheware <saurabh.gheware@nvest.in>
2026-02-07 02:18:09 +0530

    incrementing x variable

--- pattern.py
+++ pattern.py
@@ -1,6 +1,10 @@
 n = 5
 
-for i in range(1, n + 1):      # outer loop → rows
-    for j in range(i):         # inner loop → stars
+for i in range(1, n + 1): 
+    x = 0     # outer loop → rows
+    for j in range(i):    
+        x + 1     # inner loop → stars
         print("*", end="")
     print()
+
+print(x)
