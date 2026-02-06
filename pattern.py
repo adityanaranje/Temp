b1a8d95f4c15fb2ddb3e92c99270ce82191f8144
saurabhgheware <88236398+saurabhgheware@users.noreply.github.com>
2026-02-07 02:22:45 +0530

    Apply suggestion from @pr-reviewer-aditya[bot]
    Co-authored-by: pr-reviewer-aditya[bot] <255140394+pr-reviewer-aditya[bot]@users.noreply.github.com>

--- pattern.py
+++ pattern.py
@@ -3,7 +3,7 @@ n = 5
 for i in range(1, n + 1): 
     x = 0     # outer loop → rows
     for j in range(i):    
-        x + 1     # inner loop → stars
+x += 1
         print("*", end="")
     print()
 
